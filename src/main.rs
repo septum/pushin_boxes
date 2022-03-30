79417e185289d2e807af2f5b9d047d6d4b55a3eb
Orlando Valverde <orlando.jesig.va@gmail.com>
2022-03-30 11:27:53 -0600

    Open a window when running the game

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
-use bevy::prelude::*;
+use bevy::{input::system::exit_on_esc_system, prelude::*};
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Pushin Boxes".to_string(),
+            width: 640.0,
+            height: 640.0,
+            ..Default::default()
+        })
+        .insert_resource(ClearColor(Color::rgb_u8(28, 28, 28)))
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_system(exit_on_esc_system)
+        .run();
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum GameState {
+    Title,
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(UiCameraBundle::default());
 }
